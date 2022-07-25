b83ad9d262272ce5de48b3231337417737d7aaa6
rowlinson <johk2293@gmail.com>
2022-07-25 11:00:00 +0400

    Create Cat model and hide secret key in config

--- djangocats/cats/models.py
+++ djangocats/cats/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    FUNNY = 'funny'
+    SERIOUS = 'serious'
+    CUTE = 'cute'
+    CATEGORY_CHOICES = [
+        (FUNNY, 'Funny :>'),
+        (SERIOUS, 'Serious cat :|'),
+        (CUTE, 'Very cute OwO')
+    ]
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=250, blank=True)
+    photo = models.ImageField(upload_to='pictures/%Y_%m/')
+    category = models.CharField(
+        max_length=7, 
+        choices=CATEGORY_CHOICES, 
+        default=FUNNY
+        )
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_edited = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=False)
